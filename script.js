a86ed7dd9356ca53ef76b601e3676d564da06571
sayfulindmitry <sayfulindmitry@gmail.com>
2020-06-19 21:07:36 +0300

    adding starting project

---
+++ script.js
@@ -0,0 +1,18 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var css = document.querySelector("h3")
+
+function draw() {
+	body.style.background = 
+	"linear-gradient(to right, " + 
+	color1.value + ", " +
+	color2.value + ")";
+
+	css.textContent = body.style.background + ";"
+
+}
+
+color1.addEventListener("input", draw);
+color2.addEventListener("input", draw);
+
